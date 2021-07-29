1bef1babaef383bed11844a9bc390e8c13db163a
Erin McKinney <e.mckinney605@gmail.com>
2021-07-29 16:36:43 -0400

    Add project files.

---
+++ JSONChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Erin";
+            person.LastName = "McKinney";
+            person.DOB = new DateTime(1995, 10, 30);
+            person.FavoriteIceCreamFlavors = new List<string> { "Vanilla", "Cookie Dough" };
+
+            string json = JsonConvert.SerializeObject(person);
+
+            Console.WriteLine(json);
+
+            Console.ReadKey();
+        }
+    }
+}
